c124f7319d6770d695527d0550997c78e38bfa98
MadsRP <113171066+MadsRP@users.noreply.github.com>
2024-03-08 11:21:12 +0100

    Controller + Forbruger

---
+++ src/main/java/com/example/madspild/Controller.java
@@ -0,0 +1,15 @@
+package com.example.madspild;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+
+
+    @GetMapping("/forbruger")
+    public String forbruger(){
+        return "forbruger";
+    }
+
+}
